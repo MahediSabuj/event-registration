6031e34c32f4661358d0cb613326968bb7cf60bc
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2025-04-06 00:33:21 +0600

    initial commit

---
+++ src/test/java/com/ms29/event/registration/EventRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms29.event.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
